89148cae073cd138949b91dc92207aa86c3c43dc
Luca Ignatescu <lucaignatescu003@gmail.com>
2023-11-16 16:21:41 -0500

    fixed user registration 😳

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,46 +1,51 @@
-import {ErrorMessage} from '@/components/client';
-import mongoose from 'mongoose';
-import { redirect } from 'next/navigation';
-import { encode } from 'querystring';
-import bcrypt from 'bcrypt';
-import { IUser } from '@/app/_config/schemas';
+import { ErrorMessage } from "@/components/client";
+import mongoose from "mongoose";
+import { redirect } from "next/navigation";
+import { encode } from "querystring";
+import bcrypt from "bcrypt";
+import { IUser } from "@/app/_config/schemas";
 
 export default function Page() {
-
-  function handleErrorRedirect(error: string){
-    const query = encode({error});
-    return redirect('/auth/register?' + error);
-  }
-
   async function register(e: FormData) {
     "use server";
-    const User = mongoose.model<IUser>('User');
-    if (e.get('password') !== e.get('retyped')){
+    function handleErrorRedirect(error: string) {
+      const query = encode({ error });
+      redirect("/auth/register?" + query);
+    }
+
+    const User = mongoose.model<IUser>("User");
+    if (e.get("password") !== e.get("retyped")) {
       handleErrorRedirect("Passwords do not match!");
     }
-    const username:string = e.get('username') as string ?? " ";
-    const password = e.get('password') as string || " ";
+    const username: string = (e.get("username") as string) ?? " ";
+    const password = (e.get("password") as string) ?? " ";
 
-    if (await User.exists({username: username})){
+    if (await User.exists({ username: username })) {
       handleErrorRedirect("Username already exists");
     }
     /**
      * TODO: add more username checking like lowercase everything etcetcetc
      */
     const saltRounds = 10;
+    let errorCheck:string = "";
     bcrypt.hash(password, saltRounds, async (err, hash) => {
-      if (err){
+      if (err) {
         handleErrorRedirect("Server error. Please try again");
       }
-      try{
-        const newUser = new User({username, password});
+      console.log("made it here");
+      try {
+        const newUser = new User<IUser>({ username, password:hash });
         await newUser.save();
-      } catch(e){
-        handleErrorRedirect("Server error. Please try again");
+        console.log("user successfully created");
+      } catch (e) {
+        console.error(e);
+        errorCheck = "Server error. Please try again";
       }
-    })
-
-    redirect('/auth/login');
+    });
+    if (errorCheck !== ""){
+      handleErrorRedirect(errorCheck);
+    }
+    redirect("/auth/login");
   }
 
   return (
@@ -55,7 +60,7 @@ export default function Page() {
         <input name="password" id="password" type="password" required></input>
         <br></br>
         <label htmlFor="retyped">Retype password: </label>
-        <input name="retyped" id="retyped" type="retyped" required></input>
+        <input name="retyped" id="retyped" type="password" required></input>
         <br></br>
         <input type="submit" value={"Submit!"}></input>
       </form>
